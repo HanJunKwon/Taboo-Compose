d79e28283f3c5884e12d0ebe1adca0a7aba6d278
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2026-03-18 15:05:24 +0900

    TabooOutlineButton 추가

--- designsystem/src/main/java/com/kwon/taboo/compose/designsystem/button/TabooButton.kt
+++ designsystem/src/main/java/com/kwon/taboo/compose/designsystem/button/TabooButton.kt
@@ -1,5 +1,6 @@
 package com.kwon.taboo.compose.designsystem.button
 
+import androidx.compose.foundation.BorderStroke
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.hoverable
 import androidx.compose.foundation.interaction.MutableInteractionSource
@@ -18,6 +19,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.kwon.taboo.compose.designsystem.TabooBackground
 import com.kwon.taboo.compose.designsystem.ThemePreviews
@@ -38,7 +40,8 @@ fun TabooButton(
     enabled: Boolean = true,
     text: @Composable () -> Unit,
     interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
-    colors: TabooButtonColors = TabooButtonDefaults.buttonColors()
+    colors: TabooButtonColors = TabooButtonDefaults.buttonColors(),
+    border: BorderStroke? = null
 ) {
     val isPressed = interactionSource.collectIsPressedAsState()
 
@@ -61,7 +64,8 @@ fun TabooButton(
         contentColor = colors.contentColor(
             enabled = enabled,
             isPressed = isPressed.value
-        )
+        ),
+        border = border
     ) {
         Box(
             modifier = Modifier
@@ -73,30 +77,125 @@ fun TabooButton(
     }
 }
 
+@Composable
+fun TabooOutlineButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    text: @Composable () -> Unit,
+    interactionSource: MutableInteractionSource = remember { MutableInteractionSource() },
+    colors: TabooButtonColors = TabooButtonDefaults.outlineButtonColors(),
+    border: BorderStroke = TabooButtonDefaults.outlineButtonBorder(enabled)
+) = TabooButton(
+    onClick = onClick,
+    modifier = modifier,
+    enabled = enabled,
+    text = text,
+    interactionSource = interactionSource,
+    colors = colors,
+    border = border
+)
+
 object TabooButtonDefaults {
     @Composable
     fun buttonColors() = TabooButtonColors(
         containerColor = defaultContainerColor(),
         contentColor = defaultContentColor(),
-        pressedContainerColor = defaultPressedContainerColor(),
-        pressedContentColor = defaultPressedContentColor(),
+        pressedContainerColor = defaultPressContainerColor(),
+        pressedContentColor = defaultPressContentColor(),
         disableContainerColor = defaultDisableContainerColor(),
         disableContentColor = defaultDiableContentColor(),
     )
 
+    /**
+     * [TabooButton]의 배경의 기본 색상.
+     */
     private fun defaultContainerColor() = TabooBlue600
 
+    /**
+     * [TabooButton]의 콘텐츠의 기본 색상
+     */
     private fun defaultContentColor() = White
 
-    private fun defaultPressedContainerColor() = TabooBlue700
+    /**
+     * [TabooButton]의 상태가 `Pressed` 일 때 배경의 기본 색상
+     */
+    private fun defaultPressContainerColor() = TabooBlue700
 
-    private fun defaultPressedContentColor() = TabooGray200
+    /**
+     * [TabooButton]의 상태가 `Pressed`일 때, 콘텐츠의 기본 색상
+     */
+    private fun defaultPressContentColor() = TabooGray200
 
+    /**
+     * [TabooButton]이 비활성화 일 때, 배경의 기본 색상.
+     */
     @Composable
     private fun defaultDisableContainerColor() = if (isSystemInDarkTheme()) TabooBlue900 else TabooBlue200
 
+    /**
+     * [TabooButton]이 비활성화 일 때, 컨텐츠의 기본 색상.
+     */
     @Composable
     private fun defaultDiableContentColor() = if (isSystemInDarkTheme()) TabooGray300 else White
+
+    /**
+     * [TabooOutlineButton]의 생상 스키
+     */
+    @Composable
+    fun outlineButtonColors() = TabooButtonColors(
+        containerColor = defaultOutlineContainerColor(),
+        contentColor = defaultOutlineContentColor(),
+        pressedContainerColor = defaultPressOutlineContainerColor(),
+        pressedContentColor = defaultPressOutlineContentColor(),
+        disableContainerColor = defaultDisableOutlineContainerColor(),
+        disableContentColor = defaultDiableOutlineContentColor(),
+    )
+    /**
+     *
+     * [TabooOutlineButton]의 배경의 기본 색상.
+     */
+    private fun defaultOutlineContainerColor() = Color.Transparent
+
+    /**
+     * [TabooOutlineButton]의 콘텐츠의 기본 색상
+     */
+    private fun defaultOutlineContentColor() = TabooBlue600
+
+    /**
+     * [TabooOutlineButton]의 상태가 `Pressed` 일 때 배경의 기본 색상
+     */
+    private fun defaultPressOutlineContainerColor() = Color.Transparent
+
+    /**
+     * [TabooOutlineButton]의 상태가 `Pressed`일 때, 콘텐츠의 기본 색상
+     */
+    private fun defaultPressOutlineContentColor() = TabooBlue700
+
+    /**
+     * [TabooOutlineButton]이 비활성화 일 때, 배경의 기본 색상.
+     */
+    @Composable
+    private fun defaultDisableOutlineContainerColor() = Color.Transparent
+
+    /**
+     * [TabooOutlineButton]이 비활성화 일 때, 컨텐츠의 기본 색상.
+     */
+    @Composable
+    private fun defaultDiableOutlineContentColor() = if (isSystemInDarkTheme()) TabooBlue900 else TabooBlue200
+
+    /**
+     * [TabooOutlineButton]의 `BorderStroke` 두께 및 색상.
+     */
+    @Composable
+    fun outlineButtonBorder(enabled: Boolean) = BorderStroke(
+        width = 1.dp,
+        color = if (isSystemInDarkTheme()) {
+            if (enabled) TabooBlue700 else TabooBlue900
+        } else {
+            if (enabled) TabooBlue600 else TabooBlue200
+        }
+    )
 }
 
 @ThemePreviews
@@ -116,6 +215,35 @@ fun TabooButtonPreview() {
                     text = { Text(text = "Button") }
                 )
                 TabooButton(
+                    onClick = {
+
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    text = { Text(text = "Button") },
+                    enabled = false
+                )
+            }
+        }
+    }
+}
+
+@ThemePreviews
+@Composable
+fun TabooOutlineButtonPreview() {
+    TabooTheme {
+        TabooBackground {
+            Column(
+                modifier = Modifier.padding(10.dp),
+                verticalArrangement = Arrangement.spacedBy(10.dp)
+            ){
+                TabooOutlineButton(
+                    onClick = {
+
+                    },
+                    modifier = Modifier.fillMaxWidth(),
+                    text = { Text(text = "Button") }
+                )
+                TabooOutlineButton(
                     onClick = {
 
                     },
