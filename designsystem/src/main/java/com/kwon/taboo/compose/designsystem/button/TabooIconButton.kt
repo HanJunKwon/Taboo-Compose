f3f959e9819ebe3be202c39e07fadb45d1e6b00a
Han jun Kwon <hanjun.kwon.hs@gmail.com>
2026-03-24 09:22:11 +0900

    TabooIconButton 추가

---
+++ designsystem/src/main/java/com/kwon/taboo/compose/designsystem/button/TabooIconButton.kt
@@ -0,0 +1,216 @@
+package com.kwon.taboo.compose.designsystem.button
+
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.isSystemInDarkTheme
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.graphics.ColorFilter
+import androidx.compose.ui.graphics.painter.Painter
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.unit.dp
+import com.kwon.taboo.compose.designsystem.R
+import com.kwon.taboo.compose.designsystem.TabooBackground
+import com.kwon.taboo.compose.designsystem.ThemePreviews
+import com.kwon.taboo.compose.designsystem.indication.ScaleIndication
+import com.kwon.taboo.compose.designsystem.theme.TabooBlack800
+import com.kwon.taboo.compose.designsystem.theme.TabooGray100
+import com.kwon.taboo.compose.designsystem.theme.TabooGray500
+import com.kwon.taboo.compose.designsystem.theme.TabooGreen100
+import com.kwon.taboo.compose.designsystem.theme.TabooRed600
+import com.kwon.taboo.compose.designsystem.theme.TabooTheme
+import com.kwon.taboo.compose.designsystem.theme.TabooYellow100
+
+@Composable
+fun TabooIconButton(
+    onClick: () -> Unit,
+    modifier: Modifier = Modifier,
+    enabled: Boolean = true,
+    icon: Painter,
+    iconColorFilter: ColorFilter = TabooIconButtonDefaults.defaultIconColorFilter(),
+    variant: TabooIconButtonVariant = TabooIconButtonVariant.CLEAR,
+    backgroundColor: Color = TabooIconButtonDefaults.defaultBackgroundColor(variant)
+) {
+    val interactionSource = remember { MutableInteractionSource() }
+
+    Image(
+        painter = icon,
+        contentDescription = "",
+        modifier = modifier
+            .background(
+                color = backgroundColor,
+                shape = RoundedCornerShape(10.dp)
+            )
+            .border(
+                width = 1.dp,
+                color = TabooIconButtonDefaults.defaultBorderColor(variant),
+                shape = RoundedCornerShape(10.dp)
+            )
+            .padding(6.dp)
+            .clickable(
+                onClick = onClick,
+                enabled = enabled,
+                indication = ScaleIndication,
+                interactionSource = interactionSource
+            ),
+        colorFilter = iconColorFilter
+    )
+}
+
+object TabooIconButtonDefaults {
+    @Composable
+    fun defaultIconColorFilter(): ColorFilter {
+        val color = if (isSystemInDarkTheme()) TabooGray500 else TabooGray500
+        return ColorFilter.tint(
+            color = color
+        )
+    }
+
+    @Composable
+    fun defaultBackgroundColor(
+        variant: TabooIconButtonVariant,
+        isPressed: Boolean = false
+    ): Color {
+
+        return when (variant) {
+            TabooIconButtonVariant.CLEAR -> {
+                if (isPressed) {
+                    TabooGray100
+                } else {
+                    Color.Transparent
+                }
+            }
+
+            TabooIconButtonVariant.FILL -> {
+                if (isSystemInDarkTheme()) TabooBlack800 else TabooGray100
+            }
+
+            TabooIconButtonVariant.OUTLINE -> Color.Transparent
+        }
+    }
+
+    @Composable
+    fun defaultBorderColor(variant: TabooIconButtonVariant): Color {
+        return when (variant) {
+            TabooIconButtonVariant.CLEAR -> Color.Transparent
+
+            TabooIconButtonVariant.FILL -> Color.Transparent
+
+            TabooIconButtonVariant.OUTLINE -> if (isSystemInDarkTheme()) TabooBlack800 else TabooGray100
+        }
+    }
+}
+
+enum class TabooIconButtonVariant {
+    CLEAR,
+    FILL,
+    OUTLINE
+}
+
+
+@ThemePreviews
+@Composable
+private fun TabooIconButtonPreview() {
+    TabooTheme {
+        TabooBackground {
+            Column {
+                Row(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.ic_search)
+                    )
+
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.ic_search),
+                        variant = TabooIconButtonVariant.FILL
+                    )
+
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.ic_search),
+                        variant = TabooIconButtonVariant.OUTLINE
+                    )
+                }
+
+                Row(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.baseline_article_24)
+                    )
+
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.baseline_article_24),
+                        variant = TabooIconButtonVariant.FILL
+                    )
+
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.baseline_article_24),
+                        variant = TabooIconButtonVariant.OUTLINE
+                    )
+                }
+
+                Row(
+                    modifier = Modifier.padding(10.dp),
+                    horizontalArrangement = Arrangement.spacedBy(10.dp)
+                ) {
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.baseline_article_24),
+                        iconColorFilter = ColorFilter.tint(color = TabooRed600)
+                    )
+
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.baseline_article_24),
+                        iconColorFilter = ColorFilter.tint(color = TabooYellow100),
+                        variant = TabooIconButtonVariant.FILL
+                    )
+
+                    TabooIconButton(
+                        onClick = {
+
+                        },
+                        icon = painterResource(R.drawable.baseline_article_24),
+                        iconColorFilter = ColorFilter.tint(color = TabooGreen100),
+                        variant = TabooIconButtonVariant.OUTLINE
+                    )
+                }
+            }
+        }
+    }
+}
