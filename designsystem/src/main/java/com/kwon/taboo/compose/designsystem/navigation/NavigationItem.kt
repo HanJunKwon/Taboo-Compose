1cb7fb1ed5a02a56ee7ba5f02798454b59430ae7
kwonhanjun <skclaqks11@naver.com>
2026-04-07 11:14:23 +0900

    NavigationItem 클릭 이벤트 수정

--- designsystem/src/main/java/com/kwon/taboo/compose/designsystem/navigation/NavigationItem.kt
+++ designsystem/src/main/java/com/kwon/taboo/compose/designsystem/navigation/NavigationItem.kt
@@ -2,16 +2,25 @@ package com.kwon.taboo.compose.designsystem.navigation
 
 import android.util.Log
 import androidx.compose.foundation.Image
+import androidx.compose.foundation.background
+import androidx.compose.foundation.interaction.MutableInteractionSource
+import androidx.compose.foundation.interaction.collectIsPressedAsState
 import androidx.compose.foundation.isSystemInDarkTheme
 import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.RowScope
 import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.ColorFilter
 import androidx.compose.ui.graphics.painter.Painter
 import androidx.compose.ui.res.painterResource
@@ -24,9 +33,11 @@ import com.kwon.taboo.compose.designsystem.TabooBackground
 import com.kwon.taboo.compose.designsystem.ThemePreviews
 import com.kwon.taboo.compose.designsystem.scaleClickable
 import com.kwon.taboo.compose.designsystem.theme.TabooFontFamily
+import com.kwon.taboo.compose.designsystem.theme.TabooGray100
 import com.kwon.taboo.compose.designsystem.theme.TabooGray300
 import com.kwon.taboo.compose.designsystem.theme.TabooGray600
 import com.kwon.taboo.compose.designsystem.theme.TabooGray700
+import com.kwon.taboo.compose.designsystem.theme.TabooGray800
 import com.kwon.taboo.compose.designsystem.theme.TabooTheme
 
 @Composable
@@ -37,28 +48,44 @@ fun RowScope.NavigationItem(
     colors: NavigationItemColors = NavigationItemDefault.colors(),
     isSelected: Boolean = false
 ) {
-    Column(
-        modifier = Modifier
-            .padding(5.dp)
-            .scaleClickable(onClick = onClick).weight(1f),
-        horizontalAlignment = Alignment.CenterHorizontally,
-        verticalArrangement = Arrangement.spacedBy(2.dp)
-    ) {
-        Image(
-            painter = icon,
-            contentDescription = "",
-            colorFilter = ColorFilter.tint(color = colors.iconColor(isSelected))
-        )
+    val interactionSource = remember { MutableInteractionSource() }
+    val isPressed by interactionSource.collectIsPressedAsState()
 
-        Text(
-            text = text,
-            style = TextStyle(
-                fontFamily = TabooFontFamily,
-                fontWeight = FontWeight.Normal,
-                fontSize = 12.sp,
-                color = colors.textColor(isSelected)
+    Box(modifier = Modifier.weight(1f)) {
+        Column(
+            modifier = Modifier
+                .scaleClickable(
+                    onClick = onClick,
+                    interactionSource = interactionSource
+                )
+                .background(
+                    color = colors.backgroundColor(isPressed),
+                    shape = RoundedCornerShape(25)
+                )
+                .padding(5.dp)
+                .align(Alignment.Center)
+            ,
+            horizontalAlignment = Alignment.CenterHorizontally,
+            verticalArrangement = Arrangement.spacedBy(2.dp)
+        ) {
+            Image(
+                painter = icon,
+                contentDescription = "",
+                modifier = Modifier
+                    .width(20.dp),
+                colorFilter = ColorFilter.tint(color = colors.iconColor(isSelected))
             )
-        )
+
+            Text(
+                text = text,
+                style = TextStyle(
+                    fontFamily = TabooFontFamily,
+                    fontWeight = FontWeight.Normal,
+                    fontSize = 10.sp,
+                    color = colors.textColor(isSelected)
+                )
+            )
+        }
     }
 }
 
@@ -67,11 +94,15 @@ object NavigationItemDefault {
     fun colors(): NavigationItemColors {
         return if (isSystemInDarkTheme()) {
             NavigationItemColors(
+                backgroundColor = Color.Unspecified,
+                pressBackgroundColor = TabooGray800,
                 selectedColor = TabooGray300,
                 unselectedColor = TabooGray700
             )
         } else {
             NavigationItemColors(
+                backgroundColor = Color.Unspecified,
+                pressBackgroundColor = TabooGray100,
                 selectedColor = TabooGray600,
                 unselectedColor = TabooGray300
             )
@@ -98,8 +129,8 @@ fun NavigationItemPreviews() {
                     onClick = {
                         Log.d(">>> NavigationItem", "")
                     },
-                    icon = painterResource(R.drawable.ic_home),
-                    text = "홈"
+                    icon = painterResource(R.drawable.ic_favorite),
+                    text = "즐겨찾기"
                 )
             }
         }
